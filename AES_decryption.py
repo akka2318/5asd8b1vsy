0fcf366d0e1d1a33f429ac4d2acba4af802dfdb6
akka2318 <1756641922@qq.com>
2024-09-12 15:55:52 +0800

    abcdef

---
+++ AES_decryption.py
@@ -0,0 +1,51 @@
+import json
+import re
+
+from Crypto.Cipher import AES
+import base64
+
+
+def AES_Cipher():
+    key_data = {"words": [825307441, 825307441, 825307441, 825307441], "sigBytes": 16}
+
+    url = input('输入URL：')
+    index = url.find('bookDetail?')
+    base64_str = url[index+len('bookdetail?')::]
+    base64_str = base64.b64decode(base64_str).decode('utf-8')
+    base64_str = json.loads(base64_str)
+    ciphertext_b64 = base64_str['encrypt']
+    iv_data = base64_str['iv']
+
+    def convert_words_to_bytes(words, sigBytes):
+        byte_array = []
+        for word in words:
+            byte_array.append((word >> 24) & 0xFF)
+            byte_array.append((word >> 16) & 0xFF)
+            byte_array.append((word >> 8) & 0xFF)
+            byte_array.append(word & 0xFF)
+        # 截取有效字节
+        return bytes(byte_array[:sigBytes])
+
+    key = convert_words_to_bytes(key_data["words"], key_data["sigBytes"])
+    iv = convert_words_to_bytes(iv_data["words"], iv_data["sigBytes"])
+
+    # 解码 base64
+    ciphertext = base64.b64decode(ciphertext_b64)
+
+    # 解密
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    decrypted_padded = cipher.decrypt(ciphertext)
+
+    # 无填充模式
+    try:
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        decrypted = cipher.decrypt(ciphertext)
+        de = decrypted.rstrip(b'\x00').decode('utf-8', errors='ignore')
+
+        numbers = re.findall(r'\d+', de)
+        book_id = numbers[0]
+        eisbn = numbers[1]
+
+        return book_id, eisbn
+    except ValueError:
+        print("填充错误，可能是密钥、IV 或加密模式不正确。")
